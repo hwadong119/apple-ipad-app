0629a9e726733b3aebce746cba5aa3531ea70794
hwadong119 <jeonghwayun0119@naver.com>
2023-08-28 20:34:04 +0900

    iPad - 푸터 - 기타 정보와 날짜 표시, 화면 넘침 제어

--- js/main.js
+++ js/main.js
@@ -154,3 +154,7 @@ navigations.forEach(function (nav) {
 
   navigationsEl.append(mapEl)
 })
+
+
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
